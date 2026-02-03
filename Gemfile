03a23478d81a5017566087c7fb5ed73c5234c274
杨烨 <imissyou5201314@outlook.com>
2026-02-03 12:23:19 +0800

    Fix local dev setup and refresh theme base

--- Gemfile
+++ Gemfile
@@ -35,3 +35,4 @@ end
 
 
 gem "webrick", "~> 1.7"
+gem "faraday-retry"
--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,6 @@
+export PATH="/opt/homebrew/opt/ruby@3.1/bin:$PATH"
+export CPATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1"
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+
 bundle exec jekyll serve --livereload
